3bfdf9eb706ab65db0eb2d904d3590a65acc5426
mimilun <mimilun@mail.ru>
2020-09-27 18:59:50 +0300

    6. Date. Отображение даты. [#359211]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    private static Item item = new Item();
+
+    public static void main(String[] args) {
+        DateTimeFormatter outDateFormat = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String dataTimeFomat = item.getCreated().format(outDateFormat);
+        System.out.println(dataTimeFomat);
+    }
+}
