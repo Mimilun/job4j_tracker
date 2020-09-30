c918f65bcfb2bedab4c35968234847cfef54b0f4
mimilun <mimilun@mail.ru>
2020-09-30 13:11:26 +0300

    7. toString [#359218]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,8 +6,10 @@ public class StartUI {
     private static Item item = new Item();
 
     public static void main(String[] args) {
-        DateTimeFormatter outDateFormat = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String dataTimeFomat = item.getCreated().format(outDateFormat);
-        System.out.println(dataTimeFomat);
+//        DateTimeFormatter outDateFormat = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+//        String dataTimeFomat = item.getCreated().format(outDateFormat);
+//        System.out.println(dataTimeFomat);
+
+        System.out.println(item);
     }
 }
