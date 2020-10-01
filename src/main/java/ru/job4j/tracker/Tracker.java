5c7a8e8fa6ec22b91bd3154d0dce246038b99ba9
mimilun <mimilun@mail.ru>
2020-10-01 15:35:23 +0300

    7. Метод удаления заявки Tracker.delete [#359227]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,4 +57,16 @@ public class Tracker {
         return rls;
     }
 
+    public boolean delete(int id) {
+        boolean rls = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index);
+            items[size - 1] = null;
+            size--;
+            rls = true;
+        }
+        return rls;
+    }
+
 }
