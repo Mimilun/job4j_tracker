a59ccc14bf5afa3ea5953fcf0933c28f37807be0
mimilun <mimilun@mail.ru>
2020-10-01 12:47:01 +0300

    5. Tracker - хранилище [#359230]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,13 +3,17 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
-    private static Item item = new Item();
 
     public static void main(String[] args) {
 //        DateTimeFormatter outDateFormat = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 //        String dataTimeFomat = item.getCreated().format(outDateFormat);
 //        System.out.println(dataTimeFomat);
 
-        System.out.println(item);
+        Item item = new Item();
+        Tracker objTracker = new Tracker();
+        objTracker.add(item);
+
+        Item itemId = new Item();
+        System.out.println(objTracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -10,6 +12,29 @@ public class Tracker {
         items[size++] = item;
         return item;
     }
+
+    public Item[] findAll() {
+        Item[] allItems = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                allItems[size++] = items[i];
+            }
+        }
+        return allItems = Arrays.copyOf(allItems, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemByName = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getName().equals(key)) {
+                itemByName[size++] = items[i];
+            }
+        }
+        return itemByName = Arrays.copyOf(itemByName, size);
+    }
+
 
     public Item findById(int id) {
         Item rsl = null;
@@ -22,4 +47,5 @@ public class Tracker {
         }
         return rsl;
     }
+
 }
