d0fa80213f52896118ba8271031fb432e5a4d2fc
mimilun <mimilun@mail.ru>
2020-10-12 14:24:48 +0300

    9.1 Зависимость от System.out [#359148] Исправления

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,25 +11,19 @@ public class StartUI {
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
-            this.showMenu();
+            this.showMenu(actions);
 
             int select = input.askInt("Select: ");
             UserAction action = actions[select];
-            System.out.println(action.name());
             run = action.execute(input, tracker);
         }
     }
 
-    private void showMenu() {
-        System.out.println("Menu.");
-        /* добавить остальные пункты меню. */
-        System.out.println("0. Add new Item");
-        System.out.println("1. Show all items");
-        System.out.println("2. Edit item");
-        System.out.println("3. Delete item");
-        System.out.println("4. Find item by Id");
-        System.out.println("5. Find items by name");
-        System.out.println("6. Exit Program");
+    private void showMenu(UserAction[] actions) {
+        out.println("Menu.");
+        for (int index = 0; index < actions.length; index++) {
+            out.println(index + ". " + actions[index].name());
+        }
     }
 
 
