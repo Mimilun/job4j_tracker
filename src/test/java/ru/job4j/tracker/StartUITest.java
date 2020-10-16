f4b3b0ea2ff2257504c5da31f778f17b193d0087
mimilun <mimilun@mail.ru>
2020-10-16 12:35:58 +0300

    1.1. Тесты на StartUI. [#359175]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -151,4 +151,25 @@ public class StartUITest {
     }
 
 
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "11", "0"/* Пункты меню: неверный, верный.*/ }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. === Exit Program ===%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. === Exit Program ===%n"
+                )
+        ));
+    }
 }
