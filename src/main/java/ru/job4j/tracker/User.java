854c97a4d95140e36148364706aee3b2b595ab85
mimilun <mimilun@mail.ru>
2020-11-14 17:19:31 +0300

    0. Сортировка [#359127]

--- src/main/java/ru/job4j/tracker/User.java
+++ src/main/java/ru/job4j/tracker/User.java
@@ -25,11 +25,11 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+        return "User{"
+               + "id=" + id
+               + ", username='" + username + '\''
+               + ", password='" + password + '\''
+               + '}';
     }
 
 }
