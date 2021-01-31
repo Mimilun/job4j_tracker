69b3e884b3fc3318ce0fa484dc6a761617ce1c6c
mimilun <mimilun@mail.ru>
2021-01-31 19:54:42 +0300

    1. Генератор текста, версия 2.2

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -7,26 +7,20 @@ public class Article {
 
         origin = origin.replaceAll("\\p{Punct}", "");
         line = line.replaceAll("\\p{Punct}", "");
+
         String[] arrOr = origin.split(" ");
         String[] arrLine = line.split(" ");
 
         Map<String, Integer> mapOr = new HashMap<>();
-        Map<String, Integer> mapLine = new HashMap<>();
         final int value = 0;
-        boolean res = true;
 
         for (String s : arrOr) {
             mapOr.put(s, value);
         }
-        for (String s : arrLine) {
-            mapLine.put(s, value);
-            if (mapOr.containsKey(s)) {
-                mapLine.put(s, mapLine.get(s) + 1);
-            }
-        }
 
-        for (Integer i : mapLine.values()) {
-            if (i == 0) {
+        boolean res = true;
+        for (String s : arrLine) {
+            if (!mapOr.containsKey(s)) {
                 res = false;
                 break;
             }
