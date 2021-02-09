5dca501d15f0b9ce8f34712218c02ac4fb353cb6
mimilun <mimilun@mail.ru>
2021-02-09 11:25:21 +0300

    6. Функции высшего порядка [#358951]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -15,15 +16,34 @@ public class PhoneDictionary {
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
-    public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person elem : persons) {
-            if (elem.getAddress().contains(key)
-                    || elem.getName().contains(key)
-                    || elem.getPhone().contains(key)
-                    || elem.getSurname().contains(key)) {
+//    public ArrayList<Person> find(String key) {
+//        ArrayList<Person> result = new ArrayList<>();
+//        for (Person elem : persons) {
+//            if (elem.getAddress().contains(key)
+//                    || elem.getName().contains(key)
+//                    || elem.getPhone().contains(key)
+//                    || elem.getSurname().contains(key)) {
+//
+//                result.add(elem);
+//            }
+//        }
+//        return result;
+//    }
 
-                result.add(elem);
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> combineAddress = (x) -> x.getAddress().contains(key);
+        Predicate<Person> combineName = (x) -> x.getName().contains(key);
+        Predicate<Person> combinePhone = (x) -> x.getPhone().contains(key);
+        Predicate<Person> combineSurname = (x) -> x.getSurname().contains(key);
+        Predicate<Person> combine = combineAddress
+                .or(combineName)
+                .or(combinePhone)
+                .or(combineSurname);
+
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
             }
         }
         return result;
