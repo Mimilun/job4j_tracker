7c74a63a3cdcdf6aaf49c86eaaebbf3584cf9db0
mimilun <mimilun@mail.ru>
2021-02-13 11:51:10 +0300

    2. Local-Variable Type Inference [#358977]

--- src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -3,7 +3,7 @@ package ru.job4j.collection;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
